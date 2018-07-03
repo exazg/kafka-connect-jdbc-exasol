2a3f26fd722e08f14bb391419a08f279eeb2d620
exazg <exazg@users.noreply.github.com>
2018-07-03 13:31:12 +0200

    Exasol Confluent Connect JDBC dialect support

--- src/test/java/com/exasol/connect/jdbc/dialect/ExasolDatabaseDialectTest.java
+++ src/test/java/com/exasol/connect/jdbc/dialect/ExasolDatabaseDialectTest.java
@@ -1,7 +1,13 @@
 package com.exasol.connect.jdbc.dialect;
 
+import org.apache.kafka.connect.data.Date;
+import org.apache.kafka.connect.data.Decimal;
+import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Schema.Type;
+import org.apache.kafka.connect.data.Time;
+import org.apache.kafka.connect.data.Timestamp;
 import org.apache.kafka.connect.errors.ConnectException;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -31,6 +37,49 @@ public class ExasolDatabaseDialectTest extends BaseDialectTest<ExasolDatabaseDia
     // BLOB is not supported
     exception.expect(ConnectException.class);
     assertPrimitiveMapping(Type.BYTES, "BLOB");
+  }
+
+  @Test
+  public void shouldMapDecimalSchemaTypeToDecimalSqlType() {
+    assertDecimalMapping(0, "DECIMAL(36,0)");
+    assertDecimalMapping(3, "DECIMAL(36,3)");
+    assertDecimalMapping(4, "DECIMAL(36,4)");
+    assertDecimalMapping(5, "DECIMAL(36,5)");
+  }
+
+  @Test
+  public void shouldMapDataTypes() {
+    verifyDataTypeMapping("DECIMAL(3,0)", Schema.INT8_SCHEMA);
+    verifyDataTypeMapping("DECIMAL(5,0)", Schema.INT16_SCHEMA);
+    verifyDataTypeMapping("DECIMAL(10,0)", Schema.INT32_SCHEMA);
+    verifyDataTypeMapping("DECIMAL(19,0)", Schema.INT64_SCHEMA);
+    verifyDataTypeMapping("FLOAT", Schema.FLOAT32_SCHEMA);
+    verifyDataTypeMapping("DOUBLE", Schema.FLOAT64_SCHEMA);
+    verifyDataTypeMapping("BOOLEAN", Schema.BOOLEAN_SCHEMA);
+    verifyDataTypeMapping("CLOB", Schema.STRING_SCHEMA);
+    verifyDataTypeMapping("DECIMAL(36,0)", Decimal.schema(0));
+    verifyDataTypeMapping("DECIMAL(36,4)", Decimal.schema(4));
+    verifyDataTypeMapping("DATE", Date.SCHEMA);
+    verifyDataTypeMapping("TIMESTAMP", Time.SCHEMA);
+    verifyDataTypeMapping("TIMESTAMP", Timestamp.SCHEMA);
+    // BLOB is not supported
+    exception.expect(ConnectException.class);
+    verifyDataTypeMapping("BLOB", Schema.BYTES_SCHEMA);
+  }
+
+  @Test
+  public void shouldMapDateSchemaTypeToDateSqlType() {
+    assertDateMapping("DATE");
+  }
+
+  @Test
+  public void shouldMapTimeSchemaTypeToTimeSqlType() {
+    assertTimeMapping("TIMESTAMP");
+  }
+
+  @Test
+  public void shouldMapTimestampSchemaTypeToTimestampSqlType() {
+    assertTimestampMapping("TIMESTAMP");
   }
 
 }
